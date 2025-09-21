36594e202a2135ec6facc946051d1f5823d624a0
Thabang <patolebekoholdings@gmail.com>
2025-09-21 17:31:34 +0200

    Add publication_date to Book, remove Author model, update migrations

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,42 +1,32 @@
 from django.db import models
-from django.conf import settings
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
-class Author(models.Model):
-    name = models.CharField(max_length=100)
+class Library(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255, default="unknown")
 
     def __str__(self):
         return self.name
 
-
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    library = models.ForeignKey(Library, on_delete=models.CASCADE, related_name="books", default=1)
+    publication_date = models.DateField(null=True, blank=True, default='2025-01-01')
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
 
     def __str__(self):
         return self.title
 
-
-class Library(models.Model):
-    name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book)
-
-    def __str__(self):
-        return self.name
-
-
-class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
 class UserProfile(models.Model):
-    ROLE_ADMIN = "Admin"
-    ROLE_LIBRARIAN = "Librarian"
-    ROLE_MEMBER = "Member"
+    ROLE_ADMIN = "admin"
+    ROLE_LIBRARIAN = "librarian"
+    ROLE_MEMBER = "member"
 
     ROLE_CHOICES = [
         (ROLE_ADMIN, "Admin"),
@@ -44,18 +34,9 @@ class UserProfile(models.Model):
         (ROLE_MEMBER, "Member"),
     ]
 
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="userprofile")
+    user = models.OneToOneField("auth.User", on_delete=models.CASCADE)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default=ROLE_MEMBER)
 
     def __str__(self):
-        return f"{self.user.username} ({self.role})"
+        return f"{self.user.username} - {self.role}"
 
-
-# Signal to create or save UserProfile automatically when User is created
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_or_update_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
-    else:
-        # ensure profile exists on save (defensive)
-        UserProfile.objects.get_or_create(user=instance)
