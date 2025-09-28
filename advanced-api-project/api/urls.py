7eeec233e2be78321d46edffad8e1a239962131b
Thabang <patolebekoholdings@gmail.com>
2025-09-28 15:34:27 +0200

    Add explicit update and delete URL patterns for BookViewSet

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
-from .views import (
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookList, BookViewSet (
     BookListView,
     BookDetailView,
     BookCreateView,
@@ -8,6 +9,7 @@ from .views import (
 )
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('books/', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
