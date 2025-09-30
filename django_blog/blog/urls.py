889ff9cfbc09297d2145ef2a20f54287f41c1c0d
Thabang <patolebekoholdings@gmail.com>
2025-09-30 09:50:58 +0200

    We here

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,11 +18,12 @@ urlpatterns = [
 
     # profile
     path('profile/', views.profile, name='profile'),
-    path('posts/', views.PostListView.as_view(), name='post-list'),
-    path('posts/new/', PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
-    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('post/', PostListView.as_view(), name='post-list'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),   
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
 ]
 
