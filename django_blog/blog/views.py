d8bcfef75e1e3c54808d1939626f470506253b50
Thabang <patolebekoholdings@gmail.com>
2025-10-01 12:11:30 +0200

    Adding

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -155,8 +155,8 @@ class PostsByTagView(ListView):
     context_object_name = "object_list"
 
     def get_queryset(self):
-        tag_slug = self.kwargs["tag_slug"]
-        return Post.objects.filter(tags__slug=tag_slug)
+        tag_slug = self.kwargs.get["tag_slug"]
+        return Post.objects.filter(tags__slug__icontains=tag_slug)
 
 class PostSearchView(ListView):
     model = Post
